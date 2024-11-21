33048a7d1b037990e228ac4a1875bb9efe8228d3
prathambhalke <prathameshbhalke125@gmail.com>
2024-11-21 11:49:47 +0530

    learn expo

---
+++ app/(tabs)/(home)/details.tsx
@@ -0,0 +1,23 @@
+import { Link } from 'expo-router';
+import { View, Text, StyleSheet, Pressable } from 'react-native';
+
+export default function DetailsScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Details</Text>
+      <Link href="/" asChild>
+      <Pressable>
+        <Text>Home</Text>
+      </Pressable>
+    </Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
